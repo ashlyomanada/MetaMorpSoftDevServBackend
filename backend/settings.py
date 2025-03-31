1c318894c8b560524b07e3ad0109713dd9ddc625
Ashly <128467630+ashlyomanada@users.noreply.github.com>
2025-03-31 16:40:19 +0800

    can add image now

--- backend/settings.py
+++ backend/settings.py
@@ -10,7 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+# Add these at the bottom of settings.py
+import os
 from pathlib import Path
+
+# Define BASE_DIR
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
